947e5d5c4c05494e6b6debbe662b5d60a39c8c64
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-31 20:36:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,13 @@
 class Person
   attr_reader(name)
 
-#  def name
-#  @name = name
-#  end
+   def name
+   @name = name
+   end
+
+   def name=(name)
+     name
+   end 
 end
 
 #Beyonce = Person.new("Beyonce")
