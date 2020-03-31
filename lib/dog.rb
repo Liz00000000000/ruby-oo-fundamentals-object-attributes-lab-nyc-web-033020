8ab89479b66618eaf8e4268049defa7e496caa5a
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-31 20:29:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,5 @@
 class Dog
-#  attr_reader(name, breed)
-#  attr_writer(name,breed)
-#  attr_accessor(name, breed)
-  def initialize(name)
-   name
-  end
-  def name
-    name
-  end
 
-  def name=(name)
-    @name = name
-  end
 end
 
 #fido = Dog.new
